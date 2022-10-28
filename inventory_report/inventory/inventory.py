40a526e5dfd85f714668ef54c661af6997972d5d
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-28 19:33:06 -0300

    feat: class inventory initial structure

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,25 @@
+from inventory_report.importer.csv_importer import CsvImporter
+
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    __REPORT_CATALOG = {
+        "simples": SimpleReport,
+        "completo": CompleteReport,
+    }
+
+    __IMPORTER_TYPE = {
+        "csv": CsvImporter,
+        "json": "",
+        "xml": "",
+    }
+
+    @classmethod
+    def import_data(cls, file_path, report):
+        file_extension = file_path.split(".")[1]
+        file_selected = cls.__IMPORTER_TYPE.get(file_extension)
+        inventories = file_selected.import_data(file_path)
+        report_selected = cls.__REPORT_CATALOG.get(report)
+        return report_selected.generate(inventories)
