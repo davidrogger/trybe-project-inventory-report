1d9f5155e2dd1b4d720f78d6b33a5406d0dc14bd
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-31 14:51:30 -0300

    chore: change variable names

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -4,14 +4,15 @@ from inventory_report.reports.simple_report import SimpleReport
 class CompleteReport(SimpleReport):
     @classmethod
     def generate(cls, inventories):
+        simple_report = super().generate(inventories)
         companies_count = cls.count_companies(inventories)
-        company_counter = "Produtos estocados por empresa:\n"
+        additional_report = "Produtos estocados por empresa:\n"
 
         for company in inventories:
             company_name = company["nome_da_empresa"]
-            if company_name not in company_counter:
-                company_counter += (
+            if company_name not in additional_report:
+                additional_report += (
                     f"- {company_name}: {companies_count[company_name]}\n"
                 )
 
-        return f"{super().generate(inventories)}\n" f"{company_counter}"
+        return f"{simple_report}\n" f"{additional_report}"
